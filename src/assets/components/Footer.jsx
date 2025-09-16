1fd9fdba0527578cf09a1137dba62ee52051b91c
mannuccimattia <mannuccimattia@gmail.com>
2025-09-16 14:56:11 +0200

    [UPD]Done footer

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='bg-black'>
+            <div className="container text-secondary">
+                <div className="p-3 text-center">
+                    <small>Learning project made for Boolean Careers by mannuccimattia</small>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
